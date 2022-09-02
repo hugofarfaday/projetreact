1342eaa851494875be8c1f9bbfbf7511a42fd1d9
hugofarfaday <ugolescargot0601@gmail.com>
2022-09-02 10:39:36 +0200

    E3

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import HomeScreen from './components/HomeScreen/HomeScreen';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CategoryCard from './components/HomeScreen/CategoryCard/CategoryCard';
 
-function App() {
+function App(props) {
   return (
     <>
     <HomeScreen></HomeScreen>
-    <CategoryCard></CategoryCard>
+    <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU" title="Je suis un chien" user="" text="Prout"></CategoryCard>
+    <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyrhBnkQNEAxj7wCLQXFllP9vrD7FfCLiAUvbJA97gz7HbubsvxOoFjvAh21hb_UPfWEE&usqp=CAU" title="Je suis en prison" user="" text="Help"></CategoryCard>
     </>
   );
 }
--- src/components/HomeScreen/CategoryCard/CategoryCard.jsx
+++ src/components/HomeScreen/CategoryCard/CategoryCard.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card';
 
-export default class CategoryCard extends Component {
+export default class CategoryCard extends React.Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyrhBnkQNEAxj7wCLQXFllP9vrD7FfCLiAUvbJA97gz7HbubsvxOoFjvAh21hb_UPfWEE&usqp=CAU'/>
+      <Card.Img variant="top" src={this.props.img} alt={this.props.user}/>
       <Card.Body>
-        <Card.Title>Category Title</Card.Title>
+        <Card.Title>{this.props.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+            {this.props.text}
         </Card.Text>
       </Card.Body>
     </Card>
