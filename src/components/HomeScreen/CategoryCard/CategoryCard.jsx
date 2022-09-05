f6f539e6ada21570b47d9e86ca9fbe04c1bc29db
hugofarfaday <ugolescargot0601@gmail.com>
2022-09-05 13:53:59 +0200

    E4

--- src/components/HomeScreen/CategoryCard/CategoryCard.jsx
+++ src/components/HomeScreen/CategoryCard/CategoryCard.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const categorie = [
+  {title: "femme", desc: "la mode pour elle", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"},
+  {title: "homme", desc: "la mode pour lui", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"},
+  {title: "enfants", desc: "la mode pour eux", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"},
+  {title: "baby", desc: "la mode pour eux aussi", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"},
+]
+
 function CategoryCard(props) {
     return (
-      <div className='col-6'>
+      <div className='col-3'>
     <div className="card" style={{width: '18rem'}}>
       <img src={props.img} class="card-img-top" alt={props.user}/>
       <div className="card-body">
--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -8,8 +8,10 @@ class HomeScreen extends Component {
             <>
             <div className='row'>
                 <h1>LA BOUTIQUE DE UGO LEHMANN</h1>
-            <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU" title="Je suis un chien" user="" text="Prout"></CategoryCard>
-            <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyrhBnkQNEAxj7wCLQXFllP9vrD7FfCLiAUvbJA97gz7HbubsvxOoFjvAh21hb_UPfWEE&usqp=CAU" title="Je suis en prison" user="" text="Help"></CategoryCard>
+            <CategoryCard text="la mode pour lui" title="Homme" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"></CategoryCard>
+            <CategoryCard text="la mode pour elle" title="Femme" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"></CategoryCard>
+            <CategoryCard text="la mode pour eux" title="Enfants" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"></CategoryCard>
+            <CategoryCard text="la mode pour eux aussi" title="Baby" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU"></CategoryCard>
             </div>
             </> 
             );
