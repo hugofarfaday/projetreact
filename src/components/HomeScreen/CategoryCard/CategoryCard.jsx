970dae8d9de055067fd505f08f00c37663ce5764
hugofarfaday <ugolescargot0601@gmail.com>
2022-09-05 08:40:53 +0200

    E3

--- src/components/HomeScreen/CategoryCard/CategoryCard.jsx
+++ src/components/HomeScreen/CategoryCard/CategoryCard.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CategoryCard(props) {
     return (
-    <div className="card col-6" style={{width: '18rem'}}>
+      <div className='col-6'>
+    <div className="card" style={{width: '18rem'}}>
       <img src={props.img} class="card-img-top" alt={props.user}/>
       <div className="card-body">
       <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.text}</p>
       </div>
+    </div>
     </div>
     );
   }
--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -5,10 +5,14 @@ import CategoryCard from './CategoryCard/CategoryCard';
 class HomeScreen extends Component {
     render() {
         return (
+            <>
             <div className='row'>
                 <h1>LA BOUTIQUE DE UGO LEHMANN</h1>
+            <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyLTD-EEs8bxwuZtNSWPEROeaAu3FvGnlGQ6BhxGc4fuuphh9jJz1OWtJQII7BMUlrCHA&usqp=CAU" title="Je suis un chien" user="" text="Prout"></CategoryCard>
+            <CategoryCard img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyrhBnkQNEAxj7wCLQXFllP9vrD7FfCLiAUvbJA97gz7HbubsvxOoFjvAh21hb_UPfWEE&usqp=CAU" title="Je suis en prison" user="" text="Help"></CategoryCard>
             </div>
-        );
+            </> 
+            );
     }
 }
 
